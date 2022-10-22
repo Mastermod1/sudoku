30ef7f58644a2aab3a8932bdcd2e5680b1dd30ab
Mastermod1 <danielkalmucki2@gmail.com>
2022-10-23 00:34:23 +0200

    Set up new test framework

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is C++ console Sudoku game project.
+
+
+Unit Test framework: 
+https://eecs280staff.github.io/unit_test_framework
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,36 +1,8 @@
-#include <iostream>
-#include <queue>
+#include "unit_test_framework.h"
 
-std::queue<std::string> incorects;
-
-void TEST(std::string testName, bool expr)
+TEST(Sample_Test)
 {
-    if(expr)
-        std::cout<<testName<<"\t CORRECT\n";
-    else
-    {
-        std::cout<<testName<<"\t INCORRECT\n";
-        incorects.push(testName + "\t INCORRECT\n");
-    }
+    ASSERT_EQUAL(1, 1);
 }
 
-int main()
-{
-    TEST("WhenGivenTruePrintsCorrect", true);
-    TEST("WhenGivenFalsePrintsIncorrect", false);
-    TEST("WhenFalseExpPrintsIncorrect", 1 == 0);
-    if(incorects.empty())
-        std::cout << "### ALL TESTS PASSED! ALL GREEN ###\n";
-    else
-    {
-        std::cout << "###########################\nThese tests didn't pass:\n";
-        while(not incorects.empty())
-        {
-            std::cout << incorects.front();
-            incorects.pop();
-        }
-        std::cout << "### NOT ALL TESTS PASSED! ###\n";
-    }     
-        
-
-}
+TEST_MAIN()
