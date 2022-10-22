cd35e34f9d2242be89da3b0c03f2ed1d60e98184
Mastermod1 <danielkalmucki2@gmail.com>
2022-10-22 17:27:10 +0200

    Simple testing interface

---
+++ tests.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+
+std::queue<std::string> incorects;
+
+void TEST(std::string testName, bool expr)
+{
+    if(expr)
+        std::cout<<testName<<"\t CORRECT\n";
+    else
+    {
+        std::cout<<testName<<"\t INCORRECT\n";
+        incorects.push(testName + "\t INCORRECT\n");
+    }
+}
+
+int main()
+{
+    TEST("WhenGivenTruePrintsCorrect", true);
+    TEST("WhenGivenFalsePrintsIncorrect", false);
+    TEST("WhenFalseExpPrintsIncorrect", 1 == 0);
+    std::cout << "\n\n";
+    if(incorects.empty())
+        std::cout << "### ALL TESTS PASSED! ALL GREEN ###\n";
+    else
+    {
+        std::cout << "These tests didn't pass:\n";
+        while(not incorects.empty())
+        {
+            std::cout << incorects.front();
+            incorects.pop();
+        }
+        std::cout << "### NOT ALL TESTS PASSED! ###\n";
+    }     
+        
+
+}
