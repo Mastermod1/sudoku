eea1e5e7f49877f5c955db0a638bf9733edd11d3
Mastermod1 <danielkalmucki2@gmail.com>
2022-10-23 15:16:58 +0200

    Solving algorithm implemented

--- Headers/board.hpp
+++ Headers/board.hpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include "../Sources/board.cpp"
+#include <initializer_list>
 
 class Board
 {
@@ -10,9 +11,17 @@ class Board
         {
             board.fill(0);
         }
+        Board(std::array<int, 81> brd) {board = brd;};
         int cellValue(int pos) { return board[pos];}
         void setCell(int pos, int val) {if(val > 9 or val < 0) return; board[pos] = val;}
-
+        bool isFilled()
+        {
+            for(auto it : board)
+                if(it == 0)
+                    return false;
+            return true;
+        }
+        std::array<int, 81> getBoard() {return board;}
 
     private:
         std::array<int, 81> board;
--- game.cpp
+++ game.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include "Headers/display.hpp"
+#include "Headers/solver.hpp"
 
 int main()
 {
-    Board board;
+    Board board({
+    0,0,0,3,0,0,0,0,0,
+    3,0,9,7,2,6,0,0,8,
+    2,0,6,8,0,0,3,0,4,
+    0,3,2,0,7,0,5,0,0,
+    0,0,8,1,6,0,0,3,0,
+    7,0,0,0,3,8,0,0,0,
+    0,0,0,0,0,0,0,0,5,
+    9,1,0,0,0,0,0,0,3,
+    0,0,0,2,0,4,0,0,1
+    });
+    Solver solver(board);
     Display display(board);
-    for(int i = 0; i < 5; i++)
-    {
+
+
+    if(solver.solve())
         display.printBoard();
-        board.setCell(i*2, 9);
-    }
+    else
+        std::cout << "didn't solve\n";
+    //display.printBoard();
 }
