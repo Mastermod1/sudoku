5570e9177aba3d9f4ae20dec93beece3c9bdcb32
Mastermod1 <danielkalmucki2@gmail.com>
2022-10-24 20:25:31 +0200

    Implemented isFilled() method

--- Headers/board.hpp
+++ Headers/board.hpp
@@ -15,6 +15,7 @@ class Board
         Board(std::array<int, 81> givenBoard) {board = givenBoard;}
         int getCell(int pos) {return board[pos];}
         void setCell(int pos, int val);
+        bool isFilled();
         std::array<int, 81> getBoard() {return board;}
     private:
         std::array<int, 81> board;
--- Sources/board.cpp
+++ Sources/board.cpp
@@ -1,7 +1,13 @@
 #include "../Headers/board.hpp"
 
-void Board::setCell(int pos, int val){
+void Board::setCell(int pos, int val)
+{
     if(val > 9 or val < 0) 
         return; 
     board[pos] = val;
+}
+
+bool Board::isFilled()
+{
+    return std::find(board.begin(), board.end(), 0) == board.end();
 }
