a3cdd4d6092c86143840d92d59b207b6809d2855
Mastermod1 <danielkalmucki2@gmail.com>
2022-10-24 20:27:57 +0200

    Added isSolveable() and checkGameState() methods, added GameState enum

--- Headers/game.hpp
+++ Headers/game.hpp
@@ -1,14 +1,25 @@
+#pragma once
+#include <iostream>
+#include <array>
+
 #include "../Sources/board.cpp"
 #include "../Sources/input.cpp"
 #include "../Sources/display.cpp"
-#include <iostream>
 
-
+enum GameState
+{
+    InProgress = -1,
+    Won,
+    Lost
+};
 class Game
 {
     public:
         Game();
         void startGameLoop();
+        void checkGameState();
     private:
+        bool isSolveable(std::array<int, 81> grid, int row, int col);
         Board board;
+        GameState state;
 };
--- Sources/game.cpp
+++ Sources/game.cpp
@@ -2,6 +2,7 @@
 
 Game::Game()
 {
+    state = InProgress;
     board = Board({
         0,0,0,3,0,0,0,0,0,
         3,0,9,7,2,6,0,0,8,
@@ -15,16 +16,56 @@ Game::Game()
     });
 }
 
+void Game::checkGameState()
+{
+    if(board.isFilled())
+        state = Won;
+    if(not isSolveable(board.getBoard(), 0, 0))
+        state = Lost;
+}
+
 void Game::startGameLoop()
 {
     Display display(board);
     Input input(board);
+    std::array<std::string, 2> endMsg = {"GG EZ! You won!\n","LOL, so bad loser!\n"};
     int row, col, val;
-    while(true)
+    while(state == InProgress)
     {
         display.printBoard();
         std::cout << "Value: Row: Col:\n";
         std::cin >> val >> row >> col;
         input.handleInput(row, col, val);
+        checkGameState();
     }
+    std::cout << endMsg[state];
+}
+
+bool Game::isSolveable(std::array<int, 81> grid, int row, int col)
+{
+    if(row == 8 and col == 9)
+        return true;
+
+    if(col == 9)
+    {
+        row++;
+        col = 0;
+    }
+
+    int pos = tabToPos(row, col);
+
+    if(grid[pos] > 0)
+        return isSolveable(grid, row, col + 1);
+
+    for(int val = 1; val < 10; val++)
+    {
+        if(Validator(board).safeInsertOperation(row, col, val))
+        {
+            grid[pos] = val;
+            if (isSolveable(grid, row, col + 1))
+                return true;
+        }
+        grid[pos] = 0;
+    }
+    return false;
 }
