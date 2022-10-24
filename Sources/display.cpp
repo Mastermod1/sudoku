a620281d93fb20bd8c2f6e980c44bdd21144ef20
Mastermod1 <danielkalmucki2@gmail.com>
2022-10-24 18:01:51 +0200

    Upgraded visuals of printBoard()

--- Sources/display.cpp
+++ Sources/display.cpp
@@ -7,25 +7,25 @@ void Display::printBoard()
     for(int i = 1; i <= 9; i++)
     {
         if(i % 3 == 0)
-            std::cout << i - 1 << ' ';
+            std::cout << " " << i - 1 << "  ";
         else 
-            std::cout << i - 1;
+            std::cout << " " << i - 1 << " ";
     }
     std::cout << "\n";
     for(int i = 1; i <= 81; i++)
     {
         if((i - 1) % 27 == 0)
-            printHorizontalLine(9 + 4);
+            printHorizontalLine(9 * 3 + 4);
         if((i - 1) % 9 == 0)
             std::cout  << i / 9 << " ";
         if((i - 1) % 3 == 0)
             std::cout << "|";
-        std::cout << board.getCell(i - 1);
+        std::cout << " " << board.getCell(i - 1) << " ";
         if(i % 9 == 0)
             std::cout << "|\n";
         
     }
-    printHorizontalLine(9 + 4);
+    printHorizontalLine(9 * 3 + 4);
 }
 
 void Display::printHorizontalLine(int length)
